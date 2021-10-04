6b14d156a4e2be8ae542d1449961526a30e654ae
unknown <you@example.com>
2021-10-04 23:41:45 +0700

    add new monday.js tuesday.js webnesday.js and thursday folder

---
+++ Documents/week2-basic-javacript/monday.js
@@ -0,0 +1,165 @@
+
+//exercise 2. Area of a circle
+function areofacircle(){
+console.log("Arena of a circle")
+
+var pi = 3.14 ;
+typeof 3.14 //it ran !!
+//cosole.log(prin to console so that i could see)
+console.log(typeof pi);
+var radius = 5;
+console.log(typeof radius);
+
+var area = pi * radius ** 2
+console.log(area);
+var result=
+  "the arena of a circle given the radius " + radius  +  "(m) is "  +  area  +  "m2";
+var tlresult = `the arena of a circle given the radius ${radius} m is ${area} m2`;  
+  console.log(result); 
+  console.log(tlresult);
+}
+// areofacircle();  
+
+// //exercise 3. Operators & Comparison
+// part1
+function question1(){
+var question1 = 10 + 24 === 34;
+console.log(question1);
+}
+// // question1();
+
+function question2(){
+var question2 = "10" + "24" === "1024";
+console.log(typeof 1024);
+console.log(question2);
+}
+// question2();
+
+function question3(){
+  var question4 = "Hello" + " " + 2021
+}
+// question3();
+
+function question4(){
+var question4 = 1 + 2 * 3 === 7;
+console.log(question4); 
+}
+// question4();
+
+function question5(){
+var question5 =(1+3) ** 2 ===16;
+
+console.log(question5);
+}
+//  question5();
+
+function question6(){
+  var question5 = 1 / 0 ;
+  console.log
+  }
+  // question6();
+
+  function  question7(){
+    var question7 = 6 % 2 === 0;
+    console.log(question7);  
+  }
+// question7();
+
+function question8(){
+  var question8 = 5.5 % 2 === 1.5;
+  console.log(question8);
+}
+// question8();
+
+//question9
+// console.log(number("123") === 123);
+
+//question10
+// console.log(typeof(1 + "") !== String )
+
+// *part 2*
+// console.log( 5 == "5" === true); 
+// console.log( 5 === "5" === false);
+// console.log( 8 != 8.0 === false); 
+// console.log( 8 !== 8.0 === false);
+// console.log( "true" === true === false ); 
+//console.log( 4 <= 4.0 === true); 
+// console.log( 7 >= 7 === true); 
+
+// *part3
+// console.log( true && true === true); 
+// console.log( true && false === false);
+// console.log( true || true === true);
+// console.log( false || true === true);
+// console.log( !true === false);
+// console.log( !false === true);
+// console.log( (false && (true || true)) === false );
+// console.log( (false && true || true) === true);
+
+
+// exercise 4 conditinals
+// 1
+// var x=-1
+// if(x === 0){
+//   console.log(`${x} is zero`)
+// } else if(x >0 ){
+//   console.log(`${x} is a positive number `);
+// }else if (x < 0) {
+//   console.log(`${x} is a negative numver`);
+// }
+
+//  2
+//  var a = 1;
+//  var b = 4;
+//  var c = 8;
+//  if (a<b<c){
+//    console.log(`${a} is the smallest number, and ${c} is the biggest number between a = 1,b = 4, and c = 80`);
+//  }else if (b<c<a){
+//   console.log(`${b} is the smallest number, and ${a} is the biggest number between a = 1,b = 4, and c = 80`);
+//  }else if(c<a<b){
+//   console.log(`${c} is the smallest number, and ${b} is the biggest number between a = 1,b = 4, and c = 80`);
+//  }
+
+//exercise .5 Loops
+// 1
+// var a = 1;
+// var b = 5;   
+// for (var i = a; i <= b; i++ ){
+//     console.log(`output ${i}`);
+// }
+
+// // 2
+// var a = 7;
+// var b = 4;
+// for (var c= a; c >= b; c--){
+//   console.log(`outout ${c}`);
+// }
+
+
+// 6 function
+//funtion
+function saySth(){
+  console.log("Wow, it worked!!");
+}
+// console.log(saySth());
+
+function  myMovie(){
+  return  "dragon ball";
+}
+// console.log(myMovie());
+function  myName(name){
+  name = "Billy";
+  return name;
+}
+console.log(myName()+` is not my name`)
+
+   
+    
+ 
+  
+          
+
+
+
+  
+  
